bb634081c51b4ae807d9660e4396f3470cc5cea5
Dermot Treanor <dermottreanor@google.com>
2023-12-11 00:34:08 +0000

    The WriteCSVFile function is now being used by the new main2test.go file. Currently it just checks if there is a CSV file with the file name already. If not then it is created and the headers are added.

--- packages/fileshare/adjustcsvfiles.go
+++ packages/fileshare/adjustcsvfiles.go
@@ -1,2 +1,38 @@
 package fileshare
 
+import (
+	"encoding/csv"
+	"os"
+	"sort"
+)
+
+func WriteCSVFile(path string, expenseMap map[string]float64) (err error) {
+	
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil{
+		return err
+	}
+	defer file.Close()
+
+	fileInfo, err := os.Stat(path)
+	if err != nil{
+		return err
+	}
+
+	if fileInfo.Size() == 0 {
+		header := []string{}
+		for key := range expenseMap{
+			header = append(header, key)
+		}
+		sort.Strings(header)
+
+		writer := csv.NewWriter(file)
+		defer writer.Flush()
+		writer.Write(header)
+	} else{
+		writer := csv.NewWriter(file)
+		defer writer.Flush()
+	}
+
+	return nil
+}
