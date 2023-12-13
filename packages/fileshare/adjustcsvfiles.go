77f10a736957c1744e21e6c26e2fe08c1fe2bf71
Dermot Treanor <dermottreanor@google.com>
2023-12-13 09:48:15 +0000

    Made the adjustedcsvfile.go cleaner and added more comments.

--- packages/fileshare/adjustcsvfiles.go
+++ packages/fileshare/adjustcsvfiles.go
@@ -10,12 +10,7 @@ import (
 func WriteCSVFile(path string, expenseMap map[string]float64) (err error) {
 	
 	var expenseData []string
-	//Create a sorted slice of the headers
 	headers := []string{}
-		for key := range expenseMap{
-			headers = append(headers, key)
-		}
-		sort.Strings(headers)
 	
 	//Open the file and a new csv.Writer
 	file, err := os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
@@ -24,9 +19,17 @@ func WriteCSVFile(path string, expenseMap map[string]float64) (err error) {
 	}
 	defer file.Close()
 
+	//Make a sorted slice of headers.
+	for key := range expenseMap{
+		headers = append(headers, key)
+	}
+	sort.Strings(headers)
+
+	//Create the csv writer and defer a Flush call
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
+	//Get a FileInfo object and check if the array already has something in it. If not, write the headers.
 	fileInfo, err := os.Stat(path)
 	if err != nil{
 		return err
@@ -36,17 +39,14 @@ func WriteCSVFile(path string, expenseMap map[string]float64) (err error) {
 		writer.Write(headers)
 	}
 
-	//Create the string slices of expenses in the same order as we put in our headers
+	//Create a string slice of expenses in the same order that we put in our headers.
 	for _, header := range headers{
 		var singleExpense string = strconv.FormatFloat(expenseMap[header], 'f', 2, 64) 
 		expenseData = append(expenseData, singleExpense)
 
 	}
+	//Write the slice of strings. The writer will handle all of the delimitation.
 	writer.Write(expenseData)
-	
-	
-	
-	
 	
 	return nil
 }
